189491fe013e1d39f123e76973a31aacd5caaffa
윤이진 <483759@naver.com>
2021-10-25 15:52:03 +0900

    feat:[#2] 스프링부트 프로젝트 초기화

---
+++ server/src/test/java/com/aespa/nextplace/NextplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aespa.nextplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
