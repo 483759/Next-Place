e988cb72cce1a2506f6faef3b78bcdab0e6f7e7b
윤이진 <483759@naver.com>
2021-11-05 13:35:21 +0900

    feat:[#38] 빌더 메서드 재정의

--- backend/src/main/java/com/aespa/nextplace/model/entity/Pladex.java
+++ backend/src/main/java/com/aespa/nextplace/model/entity/Pladex.java
@@ -1,14 +1,16 @@
 package com.aespa.nextplace.model.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Pladex {
     @Id
     @GeneratedValue
@@ -24,4 +26,22 @@ public class Pladex {
     @Column(name = "pladex_rank")
     @Enumerated(EnumType.STRING)
     private PlamonRank rank;
+
+    public Pladex(String name, String information, PlamonRank rank) {
+        this.name = name;
+        this.information = information;
+        this.rank = rank;
+    }
+
+    @Builder
+    public Pladex(Long id, Pladex pladex) {
+        Assert.hasText(pladex.getName(), "Name must not be empty");
+        Assert.hasText(pladex.getInformation(), "Information must not be empty");
+        Assert.hasText(pladex.getRank().toString(), "Rank must not be empty");
+
+        this.id = id;
+        this.name = pladex.getName();
+        this.information = pladex.getInformation();
+        this.rank = pladex.getRank();
+    }
 }
