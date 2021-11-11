459833c4e43d6efc8de743b4ca7dffcf2120dd16
윤이진 <483759@naver.com>
2021-11-11 21:51:22 +0900

    test:[#63] 사용하지 않는 테스트 메서드 제거

--- backend/src/test/java/com/aespa/nextplace/plamon/PladexRepositoryTest.java
+++ backend/src/test/java/com/aespa/nextplace/plamon/PladexRepositoryTest.java
@@ -1,10 +1,8 @@
 package com.aespa.nextplace.plamon;
 
 import com.aespa.nextplace.model.entity.Pladex;
-import com.aespa.nextplace.model.entity.Plamon;
 import com.aespa.nextplace.model.entity.User;
 import com.aespa.nextplace.model.repository.PladexRepository;
-import com.aespa.nextplace.model.repository.PlamonRepository;
 import com.aespa.nextplace.model.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,22 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PladexRepositoryTest {
     @Autowired private PladexRepository pladexRepo;
-    @Autowired private PlamonRepository plamonRepo;
     @Autowired private UserRepository userRepo;
-
-    @DisplayName("ff")
-    @Test
-    public void ff() throws Exception {
-        //given
-        User user = userRepo.findByOauthUid("G-12345");
-
-        //when
-        List<Plamon> all = plamonRepo.findAllByUserWithNotMine(user);
-
-        //then
-        assertThat(all)
-                .isNotNull();
-    }
 
     @DisplayName("내가 가지지 않은 캐릭터 조회")
     @Test
