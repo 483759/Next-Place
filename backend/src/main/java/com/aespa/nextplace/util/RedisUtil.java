fcb54b99224a6975537469e396a4ed4079193823
shining8543 <shining8543@naver.com>
2021-11-12 17:40:54 +0900

    infra:[#81] Redis 연결 설정 추가

---
+++ backend/src/main/java/com/aespa/nextplace/util/RedisUtil.java
@@ -0,0 +1,37 @@
+package com.aespa.nextplace.util;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisUtil {
+	
+	public final long HOUR = 60L * 60; 
+	
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public String getData(String key){
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setData(String key, String value){
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        valueOperations.set(key,value);
+    }
+
+    public void setDataExpire(String key,String value,long duration){
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key,value,expireDuration);
+    }
+
+    public boolean deleteData(String key){
+        return stringRedisTemplate.delete(key);
+    }
+
+}
