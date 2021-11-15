9a5271d20ab90242fd842dd0143958742f1cf707
윤이진 <483759@naver.com>
2021-11-15 14:55:28 +0900

    test:[#91] 대표 캐릭터를 조회하는 쿼리 메서드 테스트 정의

--- backend/src/main/java/com/aespa/nextplace/model/repository/PlamonRepository.java
+++ backend/src/main/java/com/aespa/nextplace/model/repository/PlamonRepository.java
@@ -3,6 +3,8 @@ package com.aespa.nextplace.model.repository;
 import com.aespa.nextplace.model.entity.Plamon;
 import com.aespa.nextplace.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,12 @@ import java.util.List;
 @Repository
 public interface PlamonRepository extends JpaRepository<Plamon, Long> {
     List<Plamon> findAllByUser(User user);
+
     Plamon findPlamonByUserAndId(User user, Long id);
+
+    @Query("select m from Plamon m where m.user = :user and m.isMain = true")
+    Plamon findPlamonByUserAndMainIsTrue(@Param("user") User user);
+
+    @Query("select m from Plamon m where m.id = 16L")
+    Plamon findDefaultPlamon();
 }
