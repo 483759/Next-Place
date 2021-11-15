f336f46829c74d114f0ac99ad5d97fe93063f400
윤이진 <483759@naver.com>
2021-11-15 23:46:08 +0900

    feat:[#105] Service 메서드의 호출함수명, 파라미터 로깅

--- backend/src/main/java/com/aespa/nextplace/NextplaceApplication.java
+++ backend/src/main/java/com/aespa/nextplace/NextplaceApplication.java
@@ -2,8 +2,10 @@ package com.aespa.nextplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class NextplaceApplication {
 
 	public static void main(String[] args) {
