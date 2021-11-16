806327d42ac107ee67be40352082027d25b1fd50
윤이진 <483759@naver.com>
2021-11-16 14:11:13 +0900

    refactor:[#110] Util클래스에 private 생성자를 넣어 인스턴스의 생성을 제한함

--- backend/src/main/java/com/aespa/nextplace/model/request/PladexDtoAssembler.java
+++ backend/src/main/java/com/aespa/nextplace/model/request/PladexDtoAssembler.java
@@ -4,6 +4,10 @@ import com.aespa.nextplace.model.entity.Pladex;
 import com.aespa.nextplace.model.entity.PlamonRank;
 
 public class PladexDtoAssembler {
+    private PladexDtoAssembler() {
+        throw new IllegalStateException("Util 클래스는 인스턴스를 생성하지 않습니다");
+    }
+
     public static Pladex convertRequestToEntity(PladexRequest request) {
         return new Pladex(request.getName(), request.getInformation(), PlamonRank.valueOf(request.getRank()));
     }
