0facc049990e9b325dc3a42507584dcec4a1157b
윤이진 <483759@naver.com>
2021-11-18 01:32:55 +0900

    test:[#138] PladexRepository 테스트 케이스 추가

--- backend/src/test/java/com/aespa/nextplace/pladex/PladexRepositoryTest.java
+++ backend/src/test/java/com/aespa/nextplace/pladex/PladexRepositoryTest.java
@@ -1,9 +1,8 @@
 package com.aespa.nextplace.pladex;
 
 import com.aespa.nextplace.model.entity.Pladex;
-import com.aespa.nextplace.model.entity.User;
+import com.aespa.nextplace.model.entity.PlamonRank;
 import com.aespa.nextplace.model.repository.PladexRepository;
-import com.aespa.nextplace.model.repository.UserRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,23 +22,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PladexRepositoryTest {
     @Autowired private PladexRepository pladexRepo;
-    @Autowired private UserRepository userRepo;
 
-
-    @DisplayName("내가 안 가진 캐릭터 조회")
+    @DisplayName("이름으로 캐릭터 조회")
     @Test
-    void 내가_안가진_캐릭터_조회() {
+    void 이름으로_조회() throws Exception {
         //given
-        User user = userRepo.findByOauthUid("G-12345");
+        String pladexName = "성심당빵 시바";
 
         //when
-        List<Pladex> notMine = pladexRepo.findAllByUserWithNotMine(user);
+        Pladex byName = pladexRepo.findByName(pladexName);
 
         //then
-        for (Pladex pladex: notMine) {
-            System.out.println(pladex.toString());
+        assertThat(byName.getName())
+                .isEqualTo(pladexName);
+    }
+
+    @DisplayName("존재하지 않는 이름으로 캐릭터 조회")
+    @Test
+    void 없는_이름으로_조회() throws Exception {
+        //given
+        String pladexName = "없는 캐릭터";
+
+        //when
+        Pladex byName = pladexRepo.findByName(pladexName);
+
+        //then
+        assertThat(byName)
+                .isNull();
+    }
+
+    @DisplayName("등급 별 캐릭터 조회")
+    @Test
+    void 등급별_캐릭터_조회() {
+        //given
+        PlamonRank rank = PlamonRank.N;
+
+        //when
+        List<Pladex> pladexList = pladexRepo.findAllByRank(rank);
+
+        //then
+        for (Pladex pladex: pladexList) {
+            assertThat(pladex.getRank())
+                    .isEqualTo(rank);
         }
-        assertThat(notMine.size())
-                .isEqualTo(2);
+    }
+
+    @DisplayName("기본 캐릭터 조회")
+    @Test
+    void 기본캐릭터_조회() throws Exception {
+        //given
+
+        //when
+        Pladex defaultPlamon = pladexRepo.findDefaultPlamon();
+
+        //then
+        assertThat(defaultPlamon.getName())
+                .isEqualTo("기본캐릭터");
     }
 }
