d6bdda65f1f24df91bebb9d17019743f1552dc0c
윤이진 <483759@naver.com>
2021-11-18 10:22:35 +0900

    fix:[#138] FileInputStream을 finally 구문에서 close하도록 수정

--- backend/src/main/java/com/aespa/nextplace/config/FirebaseInitializer.java
+++ backend/src/main/java/com/aespa/nextplace/config/FirebaseInitializer.java
@@ -1,17 +1,16 @@
 package com.aespa.nextplace.config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Configuration
 public class FirebaseInitializer {
@@ -22,26 +21,27 @@ public class FirebaseInitializer {
 	@Bean
 	public FirebaseAuth getFirebaseAuth() throws IOException {
 
-		FileInputStream serviceAccount = new FileInputStream(keyRoute);
 		FirebaseApp firebaseApp = null;
-		List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
-		if(firebaseApps != null && !firebaseApps.isEmpty()) {
-			for(FirebaseApp app : firebaseApps) {
-				if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
-					firebaseApp = app;
-			}
-		} else {
-			FirebaseOptions options = new FirebaseOptions.Builder()
-			  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-			  .build();
-			
-			firebaseApp = FirebaseApp.initializeApp(options);
+		FileInputStream serviceAccount = new FileInputStream(keyRoute);
 
+		try {
+			List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+			if(firebaseApps != null && !firebaseApps.isEmpty()) {
+				for(FirebaseApp app : firebaseApps) {
+					if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+						firebaseApp = app;
+				}
+			} else {
+				FirebaseOptions options = new FirebaseOptions.Builder()
+						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+						.build();
+
+				firebaseApp = FirebaseApp.initializeApp(options);
+			}
+		} finally {
+			serviceAccount.close();
+			FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp);
+			return firebaseAuth;
 		}
-		
-		
-		
-		FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp);
-		return firebaseAuth;
 	}
 }
